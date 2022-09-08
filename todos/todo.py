7309831264fb0fd1f315bac44ad15afee6ae954b
karamjeetsingh.coderK <karamjeetsingh.de@gmail.com>
2022-09-08 15:53:05 +0200

    added UI

--- todos/todo.py
+++ todos/todo.py
@@ -1,33 +1,42 @@
-from fastapi import APIRouter, Path, HTTPException, status
+from fastapi import APIRouter, Path, HTTPException, status, Request, Depends
+from fastapi.templating import Jinja2Templates
+
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
+templates = Jinja2Templates(directory="templates/")
 
-@todo_router.post("/todo", status_code=201)
-async def add_todo(todo: Todo) -> dict:
+
+@todo_router.post("/todo")
+async def add_todo(request: Request, todo: Todo = Depends(Todo.as_form)):
+    todo.id = len(todo_list) + 1
     todo_list.append(todo)
-    return {
-        "message": "Todo added successfully."
-    }
+    return templates.TemplateResponse("todo.html",
+                                      {
+                                          "request": request,
+                                          "todos": todo_list
+                                      })
 
 
 @todo_router.get("/todo", response_model=TodoItems)
-async def retrieve_todo() -> dict:
-    return {
+async def retrieve_todo(request: Request):
+    return templates.TemplateResponse("todo.html", {
+        "request": request,
         "todos": todo_list
-    }
+    })
 
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(request: Request, todo_id: int = Path(..., title="The ID of the todo to retrieve.")):
     for todo in todo_list:
         if todo.id == todo_id:
-            return {
+            return templates.TemplateResponse("todo.html", {
+                "request": request,
                 "todo": todo
-            }
+            })
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Todo with supplied ID doesn't exist",
@@ -35,7 +44,8 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
 
 
 @todo_router.put("/todo/{todo_id}")
-async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated.")) -> dict:
+async def update_todo(request: Request, todo_data: TodoItem,
+                      todo_id: int = Path(..., title="The ID of the todo to be updated.")) -> dict:
     for todo in todo_list:
         if todo.id == todo_id:
             todo.item = todo_data.item
@@ -50,7 +60,7 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
 
 
 @todo_router.delete("/todo/{todo_id}")
-async def delete_single_todo(todo_id: int) -> dict:
+async def delete_single_todo(request: Request, todo_id: int) -> dict:
     for index in range(len(todo_list)):
         todo = todo_list[index]
         if todo.id == todo_id:
